ca798569e5bf00bc23e73dfd9aee223358144dcf
alessio-zamparelli <37191190+alessio-zamparelli@users.noreply.github.com>
2019-06-25 20:23:05 +0200

    Inizio della mia soluzione

--- src/it/polito/tdp/model/Model.java
+++ src/it/polito/tdp/model/Model.java
@@ -3,4 +3,6 @@ package it.polito.tdp.model;
 
 public class Model {
 	
+	
+	
 }
--- src/it/polito/tdp/model/TestModel.java
+++ src/it/polito/tdp/model/TestModel.java
@@ -3,7 +3,15 @@ package it.polito.tdp.model;
 public class TestModel {
 
 	public static void main(String[] args) {
-		
+
+		TestModel testModel = new TestModel();
+		testModel.run();
+
+	}
+
+	public void run() {
+		Model model = new Model();
+
 	}
 
 }
